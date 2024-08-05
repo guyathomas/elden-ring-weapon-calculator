f97a1dd20b972ec4a6a36bafc85d27818965315f
Guy Thomas <guythomas721@gmail.com>
2024-08-05 14:15:50 -0400

    Fix calculator

--- src/calculator/newCalculator.ts
+++ src/calculator/newCalculator.ts
@@ -7,12 +7,51 @@ type DamageTypeScaling = Partial<Record<AttackPowerType, number>>;
 const damageTypeScaling: DamageTypeScaling = {};
 type Attribute = "str" | "dex" | "int" | "fai" | "arc";
 
+const createReturnValue = ({ base }: { base: DamageTypeScaling }): DamageScalingReturnValue => ({
+  base,
+  attackPower: {
+    str: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+    dex: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+    int: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+    fai: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+    arc: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+  },
+  spellScaling: {
+    str: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+    dex: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+    int: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+    fai: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+    arc: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
+  },
+});
+
 type DamageScalingPerAttribute = Record<Attribute, DamageTypeScaling[]>;
 type DamageScalingReturnValue = {
   base: DamageTypeScaling;
   attackPower: DamageScalingPerAttribute;
   spellScaling: DamageScalingPerAttribute;
 };
+
+function calculateFinalScaling({
+  attributeCorrect,
+  attributeScaling,
+  baseAttributeScaling,
+  calcCorrectGraphValue,
+}: {
+  attributeCorrect: number | true;
+  attributeScaling: number;
+  baseAttributeScaling: number;
+  calcCorrectGraphValue: number;
+}): number {
+  let scaling;
+  if (attributeCorrect === true) {
+    scaling = attributeScaling ?? 0;
+  } else {
+    scaling = (attributeCorrect * (attributeScaling ?? 0)) / (baseAttributeScaling ?? 0);
+  }
+  return scaling * calcCorrectGraphValue;
+}
+
 export function createDamageScalingPerAttribute(
   weapon: Weapon,
   weaponUpgradeLevel: number,
@@ -21,46 +60,34 @@ export function createDamageScalingPerAttribute(
   attackPower: DamageScalingPerAttribute;
   spellScaling: DamageScalingPerAttribute;
 } {
-  const returnValue: DamageScalingReturnValue = {
-    base: {},
-    attackPower: {
-      str: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-      dex: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-      int: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-      fai: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-      arc: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-    },
-    spellScaling: {
-      str: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-      dex: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-      int: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-      fai: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-      arc: Array.from({ length: 150 }, () => ({ ...damageTypeScaling })),
-    },
-  };
-  const baseWeaponAttackForWeaponLevel = weapon.attack[weaponUpgradeLevel]; // {0: 73.84, 1: 62.400000000000006, 8: 73}
-  const scalingStatsForWeaponLevel = weapon.attributeScaling[weaponUpgradeLevel]; // {str: 0.3875, dex: 0.725, int: 0.65}
-  returnValue.base = baseWeaponAttackForWeaponLevel;
+  const base = weapon.attack[weaponUpgradeLevel]; // {0: 73.84, 1: 62.400000000000006, 8: 73}
+  const returnValue = createReturnValue({ base });
 
   for (const damageType of allDamageTypes) {
-    if (!baseWeaponAttackForWeaponLevel[damageType]) continue; // Does not have that damage type
-    const baseDamageForDamageType = baseWeaponAttackForWeaponLevel[damageType]; // 73.84
-    if (!baseDamageForDamageType) continue;
+    if (!base[damageType]) continue; // Does not have that damage type
 
     const calcCorrectGraphForDamageType = weapon.calcCorrectGraphs[damageType]; // [1: 0, 2: 0.008344518906935003, 3: 0.01917067028327579, 4: 0.031185076135726773, ...]
-
-    const scalingPairsForDamageType = Object.entries(scalingStatsForWeaponLevel).filter(
+    const scalingPairsForDamageType = Object.entries(
+      weapon.attributeScaling[weaponUpgradeLevel],
+    ).filter(
       ([attribute]) => weapon.attackElementCorrect[damageType]?.[attribute as Attribute],
     ) as [Attribute, number][];
     for (const [attribute, attributeScaling] of scalingPairsForDamageType) {
-      // [str, 0.3875], [dex, 0.725], [int, 0.65]]
+      const attributeCorrect = weapon.attackElementCorrect[damageType]?.[attribute];
+      if (!attributeCorrect) continue;
       const attributeScalingArray = returnValue.attackPower[attribute];
       for (let i = 0; i < attributeScalingArray.length; i++) {
-        const isCatalyst = Boolean(weapon.sorceryTool || weapon.incantationTool);
-        const scaling = attributeScaling * calcCorrectGraphForDamageType[i];
+        const finalScaling = calculateFinalScaling({
+          attributeCorrect,
+          attributeScaling,
+          baseAttributeScaling: weapon.attributeScaling[0][attribute] || 0,
+          calcCorrectGraphValue: calcCorrectGraphForDamageType[i],
+        });
 
-        returnValue.attackPower[attribute][i][damageType] = baseDamageForDamageType * scaling;
-        if (isCatalyst) returnValue.spellScaling[attribute][i][damageType] = 100 * scaling;
+        returnValue.attackPower[attribute][i][damageType] = base[damageType] * (finalScaling || 0);
+        if (weapon.sorceryTool || weapon.incantationTool) {
+          returnValue.spellScaling[attribute][i][damageType] = 100 * finalScaling;
+        }
       }
     }
   }
