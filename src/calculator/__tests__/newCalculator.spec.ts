563986de2aca1f824827cf1d333cfc74a738821b
Guy Thomas <guythomas721@gmail.com>
2025-01-05 10:19:07 -0500

    add test cases comparing calculators

--- src/calculator/__tests__/newCalculator.spec.ts
+++ src/calculator/__tests__/newCalculator.spec.ts
@@ -1,8 +1,13 @@
+import { type DamageAttributeValues, damageAttributes } from "../attributes";
 import {
   createDamageScalingPerAttribute,
   getIncrementalDamagePerAttribute,
 } from "../newCalculator";
+import getWeaponAttack from "../calculator";
 import { TEST_WEAPONS } from "./mocks";
+
+const sumObjectValues = (obj: Record<string, number>) =>
+  Object.values(obj).reduce((acc, v) => acc + v, 0);
 
 describe("createDamageScalingPerAttribute", () => {
   TEST_WEAPONS.forEach(({ weapon, upgradeLevel, weaponClass }) => {
@@ -26,6 +31,42 @@ describe("getIncrementalDamagePerAttribute", () => {
       it("calculates incremental damage correctly with two-handing", () => {
         const result = getIncrementalDamagePerAttribute(weapon, upgradeLevel, true);
         expect(result).toMatchSnapshot();
+      });
+
+      describe("should return the same value for both calculators", () => {
+        const TWO_HANDING = false;
+        const ATTRIBUTE_COMBINATIONS: DamageAttributeValues[] = [
+          {
+            str: 14,
+            dex: 13,
+            int: 9,
+            fai: 18,
+            arc: 7,
+          },
+        ];
+
+        const dmg = getIncrementalDamagePerAttribute(weapon, upgradeLevel, TWO_HANDING);
+        ATTRIBUTE_COMBINATIONS.forEach((attributes) => {
+          it(`should return the same value for both calculators for attributes: ${JSON.stringify(attributes)}`, () => {
+            const newDamage = damageAttributes.reduce(
+              (acc, attribute) => acc + dmg.attackPower[attribute][attributes[attribute]].total,
+              dmg.base.total,
+            );
+
+            const oldDamage = sumObjectValues(
+              getWeaponAttack({
+                weapon,
+                attributes,
+                upgradeLevel,
+                twoHanding: TWO_HANDING,
+                disableTwoHandingAttackPowerBonus: false,
+                ineffectiveAttributePenalty: 0.4,
+              }).attackPower,
+            );
+
+            expect(Math.floor(newDamage)).toEqual(Math.floor(oldDamage));
+          });
+        });
       });
     });
   });
