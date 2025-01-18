04981ef4e401073245ef67764719ad2e04fb3885
Guy Thomas <guythomas721@gmail.com>
2025-01-18 09:57:19 -0500

    fix regulation version label

--- src/app/regulationVersions.tsx
+++ src/app/regulationVersions.tsx
@@ -41,7 +41,7 @@ export interface RegulationVersion {
 
 const regulationVersions: Record<RegulationVersionName, RegulationVersion> = {
   latest: {
-    name: "Patch 1.16 (latest)",
+    name: "Patch 1.14",
     info: (
       <>
         <AlertTitle>
